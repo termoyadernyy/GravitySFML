34f9049edcb6337d34a51c62f89d60c79471ca07
tape <90852088+termoyadernyy@users.noreply.github.com>
2024-04-07 17:55:09 +0300

    Add project files.

---
+++ GravitySFML/Laws.cpp
@@ -0,0 +1,19 @@
+#include "Gravity.hpp"
+
+sf::Vector2f Sec_New_Law(sf::Vector2f F, float m) 
+{ 
+	return F / m; 
+}
+
+sf::Vector2f Law_of_Univ_Grav(Planet p1, Planet p2, float, float)
+{
+	sf::Vector2f dir = p2.get_position() - p1.get_position();
+
+	float r = std::sqrt(dir.x * dir.x + dir.y * dir.y);
+	dir.x /= r; 
+	dir.y /= r;
+
+	sf::Vector2f acceleration = Sec_New_Law(dir * ((p1.get_mass() * p2.get_mass()) / (r * r)), p1.get_mass());
+
+	return acceleration;
+}
